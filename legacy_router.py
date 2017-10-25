9dc071a3358d842d8e2f2d66e448f8a1f984d48e
Benjamin Holland <ben.holland.core@gmail.com>
2017-10-25 16:47:10 -0700

    Fixed the SPELLING ERROR THAT WAS CAUSING THE PROBLEM

--- legacy_router.py
+++ legacy_router.py
@@ -6,26 +6,31 @@ from mininet.log import setLogLevel,info
 
 def init_network():
 	#no idea what ipBase does
-	net=Mininet(topo=None,build=False,ipBase='10.0.0.0/25')
-	r1=net.addHost('r1',cls=Node,ip='10.0.0.2/25')
-	r1.cmd('sysctl -w net.ipv4.ip_foward=1')
+	net=Mininet(topo=None,build=False,ipBase='10.0.0.0/24')
+	r1=net.addHost('r1',cls=Node,ip='10.0.1.100/24')
+	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 	
 	#set up host one to be device 2 on subnetwork 1 of network 10.0.0.0		
-	h1=net.addHost('h1',cls=Host,ip='10.0.0.3/25',
- 		defaultRoute='via 10.0.0.2')
+	h1=net.addHost('h1',
+		cls=Host,
+		ip='10.0.1.10/24',
+ 		defaultRoute='via 10.0.1.100')
+	
 	#set up host two to be device 2 on subnetwork 2 of network 10.0.0.0 	
-	h2=net.addHost('h2',cls=Host,ip='10.0.0.130/25',
-		 defaultRoute='via 10.0.0.129')
+	h2=net.addHost('h2',
+		cls=Host,
+		ip='10.0.2.10/24',
+		defaultRoute='via 10.0.2.100')
 	
 	
 	#link in
 	net.addLink(h1,r1,
-		intfName1='h1eth0',params1={'ip':'10.0.0.3/25'},
-		intfName2='r1eth0',params2={'ip':'10.0.0.2/25'})
+		intfName1='h1-eth0',
+		intfName2='r1-eth0',params2={'ip':'10.0.1.100/24'})
 
 	net.addLink(h2,r1,
-		intfName1='h2eth0',params1={'ip':'10.0.0.130/25'},
-		intfName2='r1eth1',params2={'ip':'10.0.0.129/25'})
+		intfName1='h2-eth0',
+		intfName2='r1-eth1',params2={'ip':'10.0.2.100/24'})
 
 	net.build()
 
